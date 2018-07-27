fba3f0aefc6aa4f43634dc9e211e9efb907da80d
jiangsongy <jiangsongy@jumei.com>
2018-07-27 23:56:49 +0800

    new test

---
+++ src/main/java/com/yjs/commonutils/commonconfig/SwaggerConfig.java
@@ -0,0 +1,47 @@
+package com.yjs.commonutils.commonconfig;
+
+import io.swagger.models.Contact;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig
+{
+    /**
+     * 创建API
+     */
+    @Bean
+    public Docket createRestApi()
+    {
+        return new Docket(DocumentationType.SWAGGER_2)
+                // 详细定制
+                .apiInfo(apiInfo())
+                .select()
+                // 指定当前包路径
+                .apis(RequestHandlerSelectors.basePackage("com.ruoyi.project.tool.swagger"))
+                // 扫描所有 .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    /**
+     * 添加摘要信息
+     */
+    private ApiInfo apiInfo()
+    {
+        // 用ApiInfoBuilder进行定制
+        return new ApiInfoBuilder()
+                .title("标题：my swagger2 test管理系统_接口文档")
+                .description("描述：用于管理集团旗下公司的人员信息,具体包括XXX,XXX模块...")
+                .contact("swagger2")
+                .version("版本号:" + "1.0.0")
+                .build();
+    }
